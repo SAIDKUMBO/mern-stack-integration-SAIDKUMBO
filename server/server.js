a39c30254680202cae417c1afd41d911f95c318b
Said <briansaid2@gmail.com>
2025-10-25 14:38:40 +0300

    Backend fix

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? "https://YOUR-VERCEL-URL.vercel.app" // Change this to your actual production domain
+    ? "https://mern-stack-integration-saidkumbo-h1x6j9zl6-saidkumbos-projects.vercel.app" // Change this to your actual production domain
     : 'http://localhost:5173', // Vite's default port
   credentials: true
 }));
